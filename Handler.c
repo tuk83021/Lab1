c6c691fcce4ffce36d3a471ec662a95eb7dc3dfd
BlueBug <zihan9701@gmail.com>
2019-09-12 13:36:56 -0400

    got all constant num to constant name with macro defines

--- Handler.c
+++ Handler.c
@@ -10,12 +10,15 @@
 #include "Queue.c"
 #include "RandGenerator.c" // generate random num
 #include "PriorityQueue.c"
+// #include "ReadConfig.c"
 
 //defined
+#define IDLE 0
+#define BUSY 1
+
 #define QUEUESIZE 10
 #define ARRIVE_MIN 2
 #define ARRIVE_MAX 8
-#define FIN_TIME 10000
 
 //define MIN MAX
 #define CPU_MIN 50
@@ -40,17 +43,22 @@ Node queue_DISK1;
 Node queue_DISK2;
 
 int main(){
+    // Read data from file
+    // readFile();
+    // defineMacro();
+
+    // Initial event qeueu and the event
     srand(time(NULL));
     Event* task;
     PQueue* EventQueue = initializePQ();
 
-    // initial queueFIFO
-    // queue size is 10 coded in queue.c func initializeQ
-    // cpu
+    // Initial queueFIFO
+    // Queue size is 10 coded in queue.c func initializeQ()
+    // CPU
     initializeQ(&queue_CPU);
-    // disk 1
+    // DISK 1
     initializeQ(&queue_DISK1);
-    // disk 2
+    // DISK 2
     initializeQ(&queue_DISK2);
 
     // initial file log.txt
@@ -67,32 +75,32 @@ int main(){
 
     while(isEmpty(EventQueue)&&(timeGlobal < FIN_TIME)){
         task = pop(EventQueue);
-        // printQ(EventQueue);
-        // if(task->jobSeq == 7){
-        //     puts("-----");
-        //     // printQ(EventQueue);
-        //     puts("-----");
-        //     return 1;
-        // }
         usleep(500000);
         switch (task->type){
-            case 8: // arrives
-                process_CPU(task, EventQueue);
-                break;
-            case 2: // cpu start
-                process_CPU(task, EventQueue);
-                break;
-            case 3: // cpu finish
+            case ARRIVAL: // cpu arrival
                 timeGlobal = task->time;
                 process_CPU(task, EventQueue);
                 break;
-            case 4: // disk start
-                //process_DISK(task, EventQueue);
+            case CPU_BEGIN: // cpu start
+                process_CPU(task, EventQueue);
+                break;
+            case CPU_FINISH: // cpu finish
+                timeGlobal = task->time;
+                process_CPU(task, EventQueue);
+                break;
+            case DISK_ARRIVAL: // disk arrival
+                timeGlobal = task->time;
+                process_DISK(task, EventQueue);
+                break;
+            case DISK1_BEGIN: // disk start
+            case DISK2_BEGIN:
+                process_DISK(task, EventQueue);
                 break;
             // disk finish
-            case 5:
-            case 6: 
-                //process_DISK(task, EventQueue);
+            case DISK1_FINISH:
+            case DISK2_FINISH: 
+                timeGlobal = task->time;
+                process_DISK(task, EventQueue);
                 break;
             case 7: // end
                 // FILE* fp = fopen("log.txt", "a");
@@ -101,6 +109,8 @@ int main(){
                 printf("At time %-5d Job%-3d exits\n", timeGlobal, task->jobSeq);
                 break;
     } }
+    // free memory
+    destroy(EventQueue);
     return 1;
 }
 void process_Arrival(Event* task, PQueue* eventQ){
@@ -111,7 +121,7 @@ void process_Arrival(Event* task, PQueue* eventQ){
     // 1. create new job
     Event next_job;
     next_job.jobSeq = task->jobSeq + 1;
-    next_job.type = 8; // arrival
+    next_job.type = ARRIVAL; // arrival
     next_job.time = randNum(CPU_MIN, CPU_MAX) + timeGlobal;
     // next_job.time = task->time + 25; // test num
     
@@ -120,31 +130,41 @@ void process_Arrival(Event* task, PQueue* eventQ){
 }
 
 void process_CPU(Event* task, PQueue* eventQ){
-    if(task->type == 8){// arrival
+    if(task->type == ARRIVAL){// arrival
         process_Arrival(task, eventQ);
     }
 
-    if(task->type == 2){ // begin
-        
+    if(task->type == CPU_BEGIN){ // begin
+        // task finishes and exits
         Event task_fin;
         task_fin.jobSeq = task->jobSeq;
-        // task_fin.time = 40+timeGlobal;
         task_fin.time = randNum(CPU_MIN, CPU_MAX) + timeGlobal;
-        task_fin.type = 3;
+        task_fin.type = CPU_FINISH;
         push(eventQ, &task_fin);
     }
-    if(task->type == 3){ // finish
-        cpu_status = 0;
+    if(task->type == CPU_FINISH){ // finish
+        // going to disk
+        if(probabilityGet() == 0){
+            // 1. create new event for going to disk
+            Event disk_task;
+            disk_task.jobSeq = task->jobSeq;
+            disk_task.time = task->time;
+            disk_task.type = DISK_ARRIVAL;
+
+            // 2. insert the event
+            push(eventQ, &disk_task);
+        }else{
+            Event all_fin;
+            all_fin.jobSeq = task->jobSeq;
+            all_fin.time = task->time;
+            all_fin.type = END;
+            push(eventQ, &all_fin);
+        }
+        cpu_status = IDLE;
         printf("At time %-5d job%-3d finishes at CPU\n",timeGlobal, task->jobSeq );
         
-
-        Event all_fin;
-        all_fin.jobSeq = task->jobSeq;
-        all_fin.time = task->time;
-        all_fin.type = 7;
-        push(eventQ, &all_fin);
     }
-    if( (queue_CPU.current>=1) && (cpu_status) == 0){
+    if( (queue_CPU.current>=1) && (cpu_status) == IDLE){
         // 1. pop out 1st in queue cpu
         int seq = deQueue(&queue_CPU);
         printf("At time %-5d job%-3d begins at CPU\n",task->time, seq );
@@ -152,152 +172,95 @@ void process_CPU(Event* task, PQueue* eventQ){
         Event job_begin;
         job_begin.jobSeq = seq;
         job_begin.time = timeGlobal;
-        job_begin.type = 2; // begin cpu
+        job_begin.type = CPU_BEGIN; // begin cpu
 
         push(eventQ, &job_begin);
 
-        cpu_status = 1;
+        cpu_status = BUSY;
     }
 }
+void process_Arrival_DISK(Event* task, PQueue* eventQ){
+    printf("At time %-5d Job%-3d arrives at Disk\n", task->time, task->jobSeq);
+    // enqueue to cpu queue
+    int size1 = queue_DISK1.current;
+    int size2 = queue_DISK2.current;
+    if(disk1_status == IDLE && disk2_status == IDLE){
+        if(size1 > size2){
+            enQueue(&queue_DISK2, task->jobSeq);
+        }else{
+            enQueue(&queue_DISK1, task->jobSeq);
+        }
+    }else{
+        if(disk1_status == IDLE && disk2_status == BUSY){
+            enQueue(&queue_DISK1, task->jobSeq);
+        }
+        if(disk2_status == IDLE && disk1_status == BUSY){
+            enQueue(&queue_DISK2, task->jobSeq);
+        }
+    }
+    
+}
+void process_DISK(Event* task, PQueue* eventQ){
+    // // CPU arrival
+    switch (task->type)
+    {
+    case DISK_ARRIVAL:
+        process_Arrival_DISK(task, eventQ);
+        break;
+    case DISK1_BEGIN :
+    {
+        Event task_fin1;
+        task_fin1.jobSeq = task->jobSeq;
+        task_fin1.time = randNum(DISK_MIN, DISK_MAX) + timeGlobal;
+        task_fin1.type = DISK1_FINISH;
+        push(eventQ, &task_fin1);
 
-// void process_CPU(Event* task, PQueue* eventQ){
-//     // file write
-//     // FILE *fp = fopen("log.txt", "a");
-//     if(task->type == 2){ // begin
-//         // send task to cpu
-//         enQueue(&queue_CPU, task->jobSeq);
-//     }else{ // != 2
-//         // finished
-//         //idle=0
-//         cpu_status = 0; 
-//         Event task_fin;
-//         task_fin.jobSeq = task->jobSeq;
-//         task_fin.time = 40;
-//         task_fin.type = 3;
+        break;
+    }
+    case DISK2_BEGIN :
+    {
+        Event task_fin2;
+        task_fin2.jobSeq = task->jobSeq;
+        task_fin2.time = randNum(DISK_MIN, DISK_MAX) + timeGlobal;
+        task_fin2.type = DISK2_FINISH;
+        push(eventQ, &task_fin2);
 
-//         push(eventQ, &task_fin);
+        break;
+    }
+    case DISK1_FINISH:
+        enQueue(&queue_CPU, task->jobSeq);
+        disk1_status = IDLE;
+        break;
 
-//         // if(probablyGet(QUIT_PROB) == 0){
-//         //     //going to disk
-//         //     // 1. create new event for going to disk
-//         //     Event disk_task;
-//         //     disk_task.jobSeq = task->jobSeq;
-//         //     disk_task.time = randNum(DISK_MIN, DISK_MAX);
-//         //     timeGlobal+= disk_task.time;
-//         //     disk_task.type = 4;
+    case DISK2_FINISH:
+        enQueue(&queue_CPU, task->jobSeq);
+        disk2_status = IDLE;
+        break;
+    }
+    if((queue_DISK1.current >= 1) && (disk1_status == IDLE)){
+        // 1. pop out the 1st one
+        int seq = deQueue(&queue_DISK1);
+        printf("At time %-5d job%-3d begins at Disk1\n",task->time, seq );
+        // 2. create disk begins
+        Event job_begin;
+        job_begin.jobSeq = seq;
+        job_begin.time = timeGlobal;
+        job_begin.type = DISK1_BEGIN;
 
-//         //     // 2. insert the event
-//         //     push(eventQ, &disk_task);
-//         // }else{
-//         //     // fprintf(fp, "At time %-5d Job%-2d exits\n", timeGlobal, task->jobSeq);
-//         //     printf("At time %-5d Job%-2d exits\n", timeGlobal, task->jobSeq);
-//         // }
-//     }
-//     // file closed
-//     // fclose(fp);
-//     //check if cpu is idle
-//     if( (queue_CPU.current>=1) && (cpu_status) == 0){
-//         // 1. pop out 1st in queue cpu
-//         int seq = deQueue(&queue_CPU);
-//         // 2. create cpu begin
-//         Event job_begin;
-//         job_begin.jobSeq = task->jobSeq;
-//         job_begin.time = task->time;
-//         job_begin.type = 2; // begin cpu
+        push(eventQ, &job_begin);
+        disk1_status = BUSY;
+    }
+    if((queue_DISK2.current >= 1) && (disk2_status == IDLE)){
+        // 1. pop out the 1st one
+        int seq = deQueue(&queue_DISK2);
+        printf("At time %-5d job%-3d begins at Disk2\n",task->time, seq );
+        // 2. create disk begins
+        Event job_begin;
+        job_begin.jobSeq = seq;
+        job_begin.time = timeGlobal;
+        job_begin.type = DISK2_BEGIN;
 
-//         push(eventQ, &job_begin);
-
-//         cpu_status = 1;
-//     }
-// }
-
-// void process_DISK(Event* task, PQueue* eventQ){
-//     // file write
-//     FILE* fp = fopen("log.txt", "a");
-
-//     // arrive at disk section
-//     if(task->type == 4){
-//         fprintf(fp, "At time %-5d Job%d arrives at Disk\n", timeGlobal, task->jobSeq);
-//         printf("At time %-5d Job%-2d arrives at Disk\n", timeGlobal, task->jobSeq);
-//         // 1. select disk
-//         int size1 = queue_DISK1.current;
-//         int size2 = queue_DISK2.current;
-//         if(size1 < size2){
-//             // 2.1 send job to disk1
-//             enQueue(&queue_DISK1, task->jobSeq);
-//         }else{
-//             // 2.2 send job to disk2
-//             enQueue(&queue_DISK2, task->jobSeq);
-//         }
-//     }else{
-//         // finish at disk
-
-//         // finish at disk 1
-//         if(task->type == 5){
-//             fprintf(fp, "At time %-5d Job%d finishes IO at Disk1\n", timeGlobal, task->jobSeq);
-//             printf("At time %-5d Job%-2d finishes IO at Disk1\n", timeGlobal, task->jobSeq);
-//             // 1. create new event for going to disk
-//             Event task1;
-//             task1.jobSeq = task->jobSeq + 1;
-//             task1.time = randNum(DISK_MIN, DISK_MAX);
-//             timeGlobal+= task1.time;
-//             task1.type = 2;
-
-//             // 2. insert the event
-//             push(eventQ, &task1);
-
-//             // 3. set disk1 to idle
-//             disk1_status = 0;
-//         }
-//         // finish at disk 2
-//         if(task->type == 6){
-//             fprintf(fp, "At time %-5d Job%d finishes IO at Disk2\n", timeGlobal, task->jobSeq);
-//             printf("At time %-5d Job%-2d finishes IO at Disk2\n", timeGlobal, task->jobSeq);
-//             // 1. create new event for going to disk
-//             Event task2;
-//             task2.jobSeq = task->jobSeq + 1;
-//             task2.time = randNum(DISK_MIN, DISK_MAX);
-//             timeGlobal+= task2.time;
-//             task2.type = 2;
-
-//             // 2. insert the event
-//             push(eventQ, &task2);
-
-//             // 3. set disk1 to idle
-//             disk2_status = 0;
-//         }
-//     }
-//     fclose(fp);
-
-//     if((queue_DISK1.current>=1) && (disk1_status) == 0){
-//         // 1. pop out 1st in queue disk1
-//         int seq = deQueue(&queue_DISK1);
-
-//         // 2. finish the event
-//         Event job_fin;
-//         job_fin.jobSeq = seq;
-//         job_fin.type = 7;
-//         job_fin.time = randNum(DISK_MIN, DISK_MIN);
-//         timeGlobal += job_fin.time;
-//         push(eventQ, &job_fin);
-        
-//         // 3. set to busy
-//         disk1_status = 1;
-
-//     }
-//     if((queue_DISK2.current>=1) && (disk2_status) == 0){
-//         // 1. pop out 1st in queue disk2
-//         int seq = deQueue(&queue_DISK2);
-
-//         // 2. finish the event
-//         Event job_fin;
-//         job_fin.jobSeq = seq;
-//         job_fin.type = 7;
-//         job_fin.time = randNum(DISK_MIN, DISK_MIN);
-//         timeGlobal += job_fin.time;
-//         push(eventQ, &job_fin);
-        
-//         // 3. set to busy
-//         disk2_status = 1;
-//     }
-// }
+        push(eventQ, &job_begin);
+        disk2_status = BUSY;
+    }
+}
