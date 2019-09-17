dcf0d952a433fbb4763baeb64c866d2d007eb00d
BlueBug <zihan9701@gmail.com>
2019-09-17 10:16:24 -0400

    implement of writing in log file and read from config file

--- Handler.c
+++ Handler.c
@@ -10,25 +10,18 @@
 #include "Queue.c"
 #include "RandGenerator.c" // generate random num
 #include "PriorityQueue.c"
-// #include "ReadConfig.c"
+#include "ReadConfig.c"
 
 //defined
 #define IDLE 0
 #define BUSY 1
-
 #define QUEUESIZE 10
-#define ARRIVE_MIN 2
-#define ARRIVE_MAX 8
 
-//define MIN MAX
-#define CPU_MIN 50
-#define CPU_MAX 70
-#define DISK_MIN 80
-#define DISK_MAX 160 // mb/s
 
 void process_Arrival(Event* task, PQueue* eventQ);
 void process_CPU(Event* task, PQueue* eventQ);
 void process_DISK(Event* task, PQueue* eventQ);
+void fileRead();
 
 // global vars
 // idle 0
@@ -37,6 +30,18 @@ int timeGlobal = 0;
 int disk1_status = 0;
 int disk2_status = 0;
 
+int INI_TIME;
+int FIN_TIME;
+int ARRIVE_MIN;
+int ARRIVE_MAX;
+int QUIT_PROB;
+int CPU_MIN;
+int CPU_MAX;
+int DISK1_MIN;
+int DISK1_MAX;
+int DISK2_MIN;
+int DISK2_MAX;
+
 // 3 queues for CPU DISK1 DISK 2
 Node queue_CPU;
 Node queue_DISK1;
@@ -44,8 +49,7 @@ Node queue_DISK2;
 
 int main(){
     // Read data from file
-    // readFile();
-    // defineMacro();
+    fileRead();
 
     // Initial event qeueu and the event
     srand(time(NULL));
@@ -62,12 +66,13 @@ int main(){
     initializeQ(&queue_DISK2);
 
     // initial file log.txt
-    // FILE* fp = fopen("log.txt", "w");
+    FILE* fp = fopen("log.txt", "w");
+    fclose(fp);
 
     //create new to trigger
     Event job1;
     job1.jobSeq = 1;
-    job1.type = 8; // Arrival
+    job1.type = ARRIVAL; // Arrival
     // job1.time = randNum(CPU_MIN, CPU_MAX);
     job1.time = 0;
     // timeGlobal+= job1.time;
@@ -103,17 +108,23 @@ int main(){
                 process_DISK(task, EventQueue);
                 break;
             case 7: // end
-                // FILE* fp = fopen("log.txt", "a");
-                // fprintf(fp, "At time %-5d Job%d exits\n", timeGlobal, task->jobSeq);
-                // fclose(fp);
+            {
+                FILE* fp = fopen("log.txt", "a");
+                fprintf(fp, "At time %-5d Job%-3d exits\n", timeGlobal, task->jobSeq);
+                fclose(fp);
                 printf("At time %-5d Job%-3d exits\n", timeGlobal, task->jobSeq);
                 break;
+            }
     } }
     // free memory
     destroy(EventQueue);
+    destroyPtr();
     return 1;
 }
 void process_Arrival(Event* task, PQueue* eventQ){
+    FILE* file = fopen("log.txt", "a");
+    fprintf(file, "At time %-5d Job%-3d arrives\n", task->time, task->jobSeq);
+    fclose(file);
     printf("At time %-5d Job%-3d arrives\n", task->time, task->jobSeq);
     // enqueue to cpu queue
     enQueue(&queue_CPU, task->jobSeq);
@@ -130,6 +141,9 @@ void process_Arrival(Event* task, PQueue* eventQ){
 }
 
 void process_CPU(Event* task, PQueue* eventQ){
+
+    FILE* file = fopen("log.txt", "a");
+
     if(task->type == ARRIVAL){// arrival
         process_Arrival(task, eventQ);
     }
@@ -144,7 +158,7 @@ void process_CPU(Event* task, PQueue* eventQ){
     }
     if(task->type == CPU_FINISH){ // finish
         // going to disk
-        if(probabilityGet() == 0){
+        if(probabilityGet(QUIT_PROB) == 0){
             // 1. create new event for going to disk
             Event disk_task;
             disk_task.jobSeq = task->jobSeq;
@@ -162,12 +176,14 @@ void process_CPU(Event* task, PQueue* eventQ){
         }
         cpu_status = IDLE;
         printf("At time %-5d job%-3d finishes at CPU\n",timeGlobal, task->jobSeq );
+        fprintf(file, "At time %-5d job%-3d finishes at CPU\n",timeGlobal, task->jobSeq);
         
     }
     if( (queue_CPU.current>=1) && (cpu_status) == IDLE){
         // 1. pop out 1st in queue cpu
         int seq = deQueue(&queue_CPU);
         printf("At time %-5d job%-3d begins at CPU\n",task->time, seq );
+        fprintf(file, "At time %-5d job%-3d begins at CPU\n",task->time, seq);
         // 2. create cpu begin
         Event job_begin;
         job_begin.jobSeq = seq;
@@ -178,8 +194,12 @@ void process_CPU(Event* task, PQueue* eventQ){
 
         cpu_status = BUSY;
     }
+    fclose(file);
 }
 void process_Arrival_DISK(Event* task, PQueue* eventQ){
+    FILE* file = fopen("log.txt", "a");
+    fprintf(file, "At time %-5d Job%-3d arrives at Disk\n", task->time, task->jobSeq);
+    fclose(file);
     printf("At time %-5d Job%-3d arrives at Disk\n", task->time, task->jobSeq);
     // enqueue to cpu queue
     int size1 = queue_DISK1.current;
@@ -201,6 +221,8 @@ void process_Arrival_DISK(Event* task, PQueue* eventQ){
     
 }
 void process_DISK(Event* task, PQueue* eventQ){
+    FILE* file = fopen("log.txt", "a");
+
     // // CPU arrival
     switch (task->type)
     {
@@ -211,7 +233,7 @@ void process_DISK(Event* task, PQueue* eventQ){
     {
         Event task_fin1;
         task_fin1.jobSeq = task->jobSeq;
-        task_fin1.time = randNum(DISK_MIN, DISK_MAX) + timeGlobal;
+        task_fin1.time = randNum(DISK1_MIN, DISK1_MAX) + timeGlobal;
         task_fin1.type = DISK1_FINISH;
         push(eventQ, &task_fin1);
 
@@ -221,7 +243,7 @@ void process_DISK(Event* task, PQueue* eventQ){
     {
         Event task_fin2;
         task_fin2.jobSeq = task->jobSeq;
-        task_fin2.time = randNum(DISK_MIN, DISK_MAX) + timeGlobal;
+        task_fin2.time = randNum(DISK2_MIN, DISK2_MAX) + timeGlobal;
         task_fin2.type = DISK2_FINISH;
         push(eventQ, &task_fin2);
 
@@ -241,6 +263,7 @@ void process_DISK(Event* task, PQueue* eventQ){
         // 1. pop out the 1st one
         int seq = deQueue(&queue_DISK1);
         printf("At time %-5d job%-3d begins at Disk1\n",task->time, seq );
+        fprintf(file, "At time %-5d job%-3d begins at Disk1\n",task->time, seq);
         // 2. create disk begins
         Event job_begin;
         job_begin.jobSeq = seq;
@@ -254,6 +277,7 @@ void process_DISK(Event* task, PQueue* eventQ){
         // 1. pop out the 1st one
         int seq = deQueue(&queue_DISK2);
         printf("At time %-5d job%-3d begins at Disk2\n",task->time, seq );
+        fprintf(file, "At time %-5d job%-3d begins at Disk2\n",task->time, seq);
         // 2. create disk begins
         Event job_begin;
         job_begin.jobSeq = seq;
@@ -263,4 +287,19 @@ void process_DISK(Event* task, PQueue* eventQ){
         push(eventQ, &job_begin);
         disk2_status = BUSY;
     }
+    fclose(file);
+}
+void fileRead(){
+    readFile();
+    INI_TIME = getIniTime();
+    FIN_TIME = getFinTime();
+    ARRIVE_MIN = getArrivalMin();
+    ARRIVE_MAX = getArrivalMax();
+    QUIT_PROB = getQuitProb();
+    CPU_MIN = getCPUMin();
+    CPU_MAX = getCPUMax();
+    DISK1_MIN = getDisk1Min();
+    DISK1_MAX = getDisk1Max();
+    DISK2_MIN = getDisk2Min();
+    DISK2_MAX = getDisk2Max();
 }
